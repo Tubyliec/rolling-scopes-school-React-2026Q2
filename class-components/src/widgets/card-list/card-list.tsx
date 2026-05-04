fce5771bcec469f24f031dbd46d76c4e7f28015a
tubyliec <antikruk@vivaldi.net>
2026-05-04 20:06:05 +0300

    feat: add card-list component

--- class-components/src/widgets/card-list/card-list.tsx
+++ class-components/src/widgets/card-list/card-list.tsx
@@ -1,34 +1,34 @@
-import {Component, type JSX} from 'react';
+import { Component, type JSX } from 'react';
 import type { CardListProps } from './model/interfaces/card-list.interface';
 
 import './card-list.scss';
-import Card from "../../entities/person/ui/card/card.tsx";
+import Card from '../../entities/person/ui/card/card.tsx';
 
 class CardList extends Component<CardListProps> {
-    private renderEmptyState(): JSX.Element {
-        return (
-            <div className="card-list__empty">
-                <div className="card-list__empty-glyph">◈</div>
-                NO RECORDS FOUND
-            </div>
-        );
+  private renderEmptyState(): JSX.Element {
+    return (
+      <div className="card-list__empty">
+        <div className="card-list__empty-glyph">◈</div>
+        NO RECORDS FOUND
+      </div>
+    );
+  }
+
+  public render(): JSX.Element {
+    const { results } = this.props;
+
+    if (!results.length) {
+      return this.renderEmptyState();
     }
 
-    public render(): JSX.Element {
-        const { results } = this.props;
-
-        if (!results.length) {
-            return this.renderEmptyState();
-        }
-
-        return (
-            <div className="card-list">
-                {results.map((person, index) => (
-                    <Card key={index} person={person} />
-                ))}
-            </div>
-        );
-    }
+    return (
+      <div className="card-list">
+        {results.map((person, index) => (
+          <Card key={index} person={person} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default CardList;
