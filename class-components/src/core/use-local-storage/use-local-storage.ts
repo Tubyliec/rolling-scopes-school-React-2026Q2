e4cffe6638e992390411d574faed65b030ccc4f6
tubyliec <antikruk@vivaldi.net>
2026-05-17 19:57:50 +0300

    feat: add local storage service

---
+++ class-components/src/core/use-local-storage/use-local-storage.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import type { LSRequest } from './model/interfaces/ls-request.interface';
+import type { LSResponse } from './model/interfaces/ls-response.interface';
+
+export function useLocalStorage({ key, initialValue }: LSRequest): LSResponse {
+  const [storedValue, setStoredValue] = useState<string>(
+    () => localStorage.getItem(key) ?? initialValue
+  );
+
+  const setValue = (value: string): void => {
+    setStoredValue(value);
+    localStorage.setItem(key, value);
+  };
+
+  return { value: storedValue, setValue };
+}
