44c27c59dbb8a205e7704e3b004f8d86898110c6
tubyliec <antikruk@vivaldi.net>
2026-05-17 21:28:33 +0300

    refactor: refactor Spinner component into a functional component

--- class-components/src/shared/ui/spinner/spinner.tsx
+++ class-components/src/shared/ui/spinner/spinner.tsx
@@ -1,15 +1,13 @@
-import { Component, type JSX } from 'react';
+import type { JSX } from 'react';
 import './spinner.scss';
 
-class Spinner extends Component {
-  public render(): JSX.Element {
-    return (
-      <div className="spinner">
-        <div className="spinner__track" />
-        <span className="spinner__label">LOADING DATA</span>
-      </div>
-    );
-  }
+function Spinner(): JSX.Element {
+  return (
+    <div className="spinner">
+      <div className="spinner__track" />
+      <span className="spinner__label">LOADING DATA</span>
+    </div>
+  );
 }
 
 export default Spinner;
