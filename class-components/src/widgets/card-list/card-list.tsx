4075bf856660e4a6c43aefe998aa1e299420c53e
tubyliec <antikruk@vivaldi.net>
2026-05-17 23:37:54 +0300

    refactor: refactor card and card-list section component into a functional component

--- class-components/src/widgets/card-list/card-list.tsx
+++ class-components/src/widgets/card-list/card-list.tsx
@@ -1,11 +1,10 @@
-import { Component, type JSX } from 'react';
+import type { JSX } from 'react';
 import type { CardListProps } from './model/interfaces/card-list.interface';
-
 import './card-list.scss';
 import Card from '../../entities/person/ui/card/card.tsx';
 
-class CardList extends Component<CardListProps> {
-  private renderEmptyState(): JSX.Element {
+function CardList({ results }: CardListProps): JSX.Element {
+  if (!results.length) {
     return (
       <div className="card-list__empty">
         <div className="card-list__empty-glyph">◈</div>
@@ -14,21 +13,13 @@ class CardList extends Component<CardListProps> {
     );
   }
 
-  public render(): JSX.Element {
-    const { results } = this.props;
-
-    if (!results.length) {
-      return this.renderEmptyState();
-    }
-
-    return (
-      <div className="card-list">
-        {results.map((person, index) => (
-          <Card key={index} person={person} />
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div className="card-list">
+      {results.map((person, index) => (
+        <Card key={index} person={person} />
+      ))}
+    </div>
+  );
 }
 
 export default CardList;
