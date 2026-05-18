2aa01cbd0eaf7d8062c0f842d41a599514c4e907
tubyliec <antikruk@vivaldi.net>
2026-05-18 22:50:33 +0300

    feat: add link to the header

--- class-components/src/widgets/header/header.tsx
+++ class-components/src/widgets/header/header.tsx
@@ -1,16 +1,27 @@
-import { Component, type JSX } from 'react';
+import type { JSX } from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.scss';
 
-class Header extends Component {
-  public render(): JSX.Element {
-    return (
+function Header(): JSX.Element {
+  return (
+    <header className="app-header">
       <div className="header__wrapper wrapper">
-        <header className="app-header">
-          <p className="app-header__logo">Star Wars API search</p>
-        </header>
+        <NavLink to="/main" className="app-header__logo">
+          Star Wars API search
+        </NavLink>
+        <nav className="app-header__nav">
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              `app-header__nav-link${isActive ? ' app-header__nav-link--active' : ''}`
+            }
+          >
+            About
+          </NavLink>
+        </nav>
       </div>
-    );
-  }
+    </header>
+  );
 }
 
 export default Header;
