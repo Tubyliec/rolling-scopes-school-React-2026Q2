1fe92f09b9b5af28d6993b7f3600f059a3575b4c
tubyliec <antikruk@vivaldi.net>
2026-05-31 15:55:06 +0300

    feat: add query client provider

--- class-components/src/app.tsx
+++ class-components/src/app.tsx
@@ -1,8 +1,14 @@
 import AppRouter from '@core/router/app-router.tsx';
 import { type JSX } from 'react';
 
+import { ThemeProvider } from '@/core/theme/theme-context.tsx';
+
 function App(): JSX.Element {
-  return <AppRouter />;
+  return (
+    <ThemeProvider>
+      <AppRouter />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- class-components/src/index.tsx
+++ class-components/src/index.tsx
@@ -1,11 +1,13 @@
 import './index.scss';
 
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/app.tsx';
 import ErrorBoundary from '@/core/error-boundary/error-boundary.tsx';
-import { ThemeProvider } from '@/core/theme/theme-context.tsx';
+import { queryClient } from '@/core/query/query-client.ts';
 
 const container = document.getElementById('root');
 
@@ -15,10 +17,12 @@ if (container === null) {
 
 createRoot(container).render(
   <StrictMode>
-    <ThemeProvider>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </ThemeProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 );
