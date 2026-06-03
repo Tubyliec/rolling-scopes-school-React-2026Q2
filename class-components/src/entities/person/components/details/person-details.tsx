7b2f3c0a1fdc735f5ba68d3b64b24a753f4d158d
tubyliec <antikruk@vivaldi.net>
2026-06-03 23:55:58 +0300

    refactor: refactor person-details

--- class-components/src/entities/person/components/details/person-details.tsx
+++ class-components/src/entities/person/components/details/person-details.tsx
@@ -19,56 +19,6 @@ function PersonDetail(): JSX.Element {
     navigate(`${AppRoute.Main}/${page}`);
   };
 
-  const renderContent = (): JSX.Element => {
-    if (isLoading) {
-      return <Spinner />;
-    }
-
-    if (error !== null) {
-      return <ErrorDisplay message={error.message} title="" />;
-    }
-
-    if (!person) {
-      return <div className="person-details__empty">No data available.</div>;
-    }
-
-    return (
-      <div className="person-details__content">
-        <h2 className="person-details__name">{person.name}</h2>
-        <ul className="person-details__list">
-          <li className="person-details__item">
-            <span className="person-details__label">Height</span>
-            <span className="person-details__value">{person.height} cm</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Mass</span>
-            <span className="person-details__value">{person.mass} kg</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Birth Year</span>
-            <span className="person-details__value">{person.birth_year}</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Gender</span>
-            <span className="person-details__value">{person.gender}</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Hair</span>
-            <span className="person-details__value">{person.hair_color}</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Eyes</span>
-            <span className="person-details__value">{person.eye_color}</span>
-          </li>
-          <li className="person-details__item">
-            <span className="person-details__label">Skin</span>
-            <span className="person-details__value">{person.skin_color}</span>
-          </li>
-        </ul>
-      </div>
-    );
-  };
-
   return (
     <aside className="person-details">
       <div className="person-details__actions">
@@ -77,7 +27,44 @@ function PersonDetail(): JSX.Element {
           ✕
         </button>
       </div>
-      {renderContent()}
+      {isLoading && <Spinner />}
+      {error && <ErrorDisplay message={error.message} title="" />}
+      {!isLoading && !error && !person && <div>No data available.</div>}
+      {person && (
+        <div className="person-details__content">
+          <h2 className="person-details__name">{person.name}</h2>
+          <ul className="person-details__list">
+            <li className="person-details__item">
+              <span className="person-details__label">Height</span>
+              <span className="person-details__value">{person.height} cm</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Mass</span>
+              <span className="person-details__value">{person.mass} kg</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Birth Year</span>
+              <span className="person-details__value">{person.birth_year}</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Gender</span>
+              <span className="person-details__value">{person.gender}</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Hair</span>
+              <span className="person-details__value">{person.hair_color}</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Eyes</span>
+              <span className="person-details__value">{person.eye_color}</span>
+            </li>
+            <li className="person-details__item">
+              <span className="person-details__label">Skin</span>
+              <span className="person-details__value">{person.skin_color}</span>
+            </li>
+          </ul>
+        </div>
+      )}
     </aside>
   );
 }
