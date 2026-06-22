f52152cdb4373b39828e3195b8fdc754c76248b8
tubyliec <antikruk@vivaldi.net>
2026-06-22 22:06:49 +0300

    chore: change config files

--- class-components/eslint.config.js
+++ class-components/eslint.config.js
@@ -1,68 +1,76 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import reactPlugin from 'eslint-plugin-react';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import next from '@next/eslint-plugin-next';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config({
-  ignores: ['dist', 'node_modules'],
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    react: reactPlugin,
-    'simple-import-sort': simpleImportSort,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', '.next', 'next-env.d.ts'],
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    ...reactPlugin.configs.recommended.rules,
-    ...reactPlugin.configs['jsx-runtime'].rules,
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          [
-            '^react\\u0000$',
-            '^react$',
-            '^react-router-dom$',
-            '^react-dom',
-            '^react-hook-form',
-          ],
-          ['^@tanstack'],
-          ['^@core'],
-          ['^@/core'],
-          ['^@entities'],
-          ['^@/entities'],
-          ['^@features'],
-          ['^@/features'],
-          ['^@pages'],
-          ['^@/pages'],
-          ['^@widgets'],
-          ['^@/widgets'],
-          ['^@shared'],
-          ['^@/shared'],
-          ['^@'],
-          ['^.+\\u0000$'],
-          ['^\\.'],
-        ],
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+
+    plugins: {
+      '@next/next': next,
+      'simple-import-sort': simpleImportSort,
+    },
+
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
       },
-    ],
-    'simple-import-sort/exports': 'error',
-  },
-  settings: {
-    react: {
-      version: 'detect',
+    },
+
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
+
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            [
+              '^react\\u0000$',
+              '^react$',
+              '^react-router-dom$',
+              '^react-dom',
+              '^react-hook-form',
+            ],
+            ['^next'],
+            ['^next-intl'],
+            ['^@tanstack'],
+            ['^@core'],
+            ['^@/core'],
+            ['^@entities'],
+            ['^@/entities'],
+            ['^@features'],
+            ['^@/features'],
+            ['^@pages'],
+            ['^@/pages'],
+            ['^@widgets'],
+            ['^@/widgets'],
+            ['^@shared'],
+            ['^@/shared'],
+            ['^@'],
+            ['^.+\\u0000$'],
+            ['^\\.'],
+          ],
+        },
+      ],
+
+      'simple-import-sort/exports': 'error',
     },
   },
-  languageOptions: {
-    globals: globals.browser,
-  },
-});
+
+  eslintConfigPrettier
+);
