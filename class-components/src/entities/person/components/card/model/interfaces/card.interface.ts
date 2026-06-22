889788431223ac7d2419aeca18961922627e49b6
tubyliec <antikruk@vivaldi.net>
2026-06-23 01:11:37 +0300

    fix: delete unused components

--- class-components/src/entities/person/components/card/model/interfaces/card.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { Person } from '@entities/person/model/types/person.type.ts';
-
-export interface CardProps {
-  readonly person: Person;
-}
--- class-components/src/widgets/card-list/model/interfaces/card-list.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { Person } from '@entities/person/model/types/person.type.ts';
-
-export interface CardListProps {
-  readonly results: Person[];
-}
