87e28e725aef165018bcb338a77c752e7ccbff28
tubyliec <antikruk@vivaldi.net>
2026-06-29 22:49:38 +0300

    refactor: refactor interfases

--- class-components/src/app/root-layout-provider.tsx
+++ class-components/src/app/root-layout-provider.tsx
@@ -7,9 +7,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@core/query/query-client';
 import { ThemeProvider } from '@core/theme/theme-context';
 
-interface RootLayoutProviderProps {
-  readonly children: ReactNode;
-}
+import type { RootLayoutProviderProps } from './model/interfaces/root-layout-provider-props.interface.ts';
 
 export function RootLayoutProvider({
   children,
